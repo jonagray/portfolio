02272582f1ef6bc3ad4b1fb9066e238ce89fe990
Jonny Graybill <jon.a.graybill@gmail.com>
2019-04-15 21:44:06 -0700

    server

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.static('./public'));
+
+app.get('/', (request, response) => response.send('index'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('<h1>Henlo Fren</h1>')
+});
+
+app.get('/data', (request, response) => {
+  let airplane = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well trained'
+  }
+
+  response.status(200).json(airplane);
+});
+
+app.use('*', (request, response) => {
+  response.send('Sorry, that route don\'t exis');
+});
+
+app.listen(PORT, () => console.log(`My FIRST Server is listening on PORT ${PORT}`));
